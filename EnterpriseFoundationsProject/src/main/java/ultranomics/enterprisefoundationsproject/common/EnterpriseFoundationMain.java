10d627c0fb4e9bd127f792a0cf9f5b40b0a2587d
NMooney <nmooney96@hotmail.com>
2022-09-02 00:31:53 -0400

    Hello World - Maven initial run
    Created Main Method to initiate Maven

---
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/common/EnterpriseFoundationMain.java
@@ -0,0 +1,8 @@
+
+package ultranomics.enterprisefoundationsproject.common;
+
+public class EnterpriseFoundationMain {
+    public static void main(String[] args){
+        System.out.println("Hellow World!");
+    }
+}
