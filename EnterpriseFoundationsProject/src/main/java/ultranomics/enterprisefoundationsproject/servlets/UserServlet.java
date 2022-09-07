fd35460325a455e382d17ec33c4ae54c5c0aec6c
NMooney <nmooney96@hotmail.com>
2022-09-07 19:27:20 -0400

    Update UserServlet.java
    Updated imports
    
    Started use of UserService class

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/servlets/UserServlet.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/servlets/UserServlet.java
@@ -8,22 +8,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import ultranomics.enterprisefoundationsproject.DAOs.UserDAO;
-import ultranomics.enterprisefoundationsproject.datamodels.User;
+import ultranomics.enterprisefoundationsproject.DTOs.UserDTO;
+import ultranomics.enterprisefoundationsproject.services.UserService;
 
 public class UserServlet extends HttpServlet{
     
-    private final UserDAO userDAO;
+    private final UserService userServ;
     
-    public UserServlet(UserDAO importUserDAO){
-        this.userDAO = importUserDAO;
+    public UserServlet(UserService importUserServ){
+        this.userServ = importUserServ;
     }
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ObjectMapper jsonMapper = new ObjectMapper();
         resp.setContentType("application/json");
-        List<User> allUsers = userDAO.getAllUsers();
+        List<UserDTO> allUsers = userServ.getAllUsers();
         
         resp.getWriter().write(jsonMapper.writeValueAsString(allUsers));
     }
