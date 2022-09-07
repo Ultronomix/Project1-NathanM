274713c15b578b321220062244091e2aca946e5b
NMooney <nmooney96@hotmail.com>
2022-09-07 15:57:34 -0400

    EnterpriseFoundationMain moved and updated
    File moved when common folder removed, updated to include Userx5 classes

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/EnterpriseFoundationMain.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/EnterpriseFoundationMain.java
@@ -1,10 +1,11 @@
 
-package ultranomics.enterprisefoundationsproject.common;
+package ultranomics.enterprisefoundationsproject;
 
-import ultranomics.enterprisefoundationsproject.common.datasource.ConnectionFactory;
-import java.sql.Connection;
+import ultranomics.enterprisefoundationsproject.servlets.UserServlet;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import ultranomics.enterprisefoundationsproject.DAOs.UserDAO;
+import ultranomics.enterprisefoundationsproject.services.UserService;
 
 public class EnterpriseFoundationMain {
     public static void main(String[] args) throws LifecycleException{
@@ -15,10 +16,14 @@ public class EnterpriseFoundationMain {
         webServer.setPort(5000); //default is 8080
         webServer.getConnector(); //formality, connects server requests to application
         
+        UserDAO userDAO = new UserDAO();
+        UserService userServ = new UserService(userDAO);
+        
         
         final String rootContext = "/p1";
         webServer.addContext(rootContext, docBase);
         webServer.addServlet(rootContext, "UserServlet", new UserServlet()).addMapping("/users");
+        webServer.addServlet(rootContext, "ReimbursementServlet", new UserServlet()).addMapping("/reimbursements");
         
         
         webServer.start();
