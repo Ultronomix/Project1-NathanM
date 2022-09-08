a2ad8aa7834ec1d37834181157c1acf1de204cf3
NMooney <nmooney96@hotmail.com>
2022-09-07 23:42:03 -0400

    Update UserDTO.java
    Added field assignment from import User to constructor
    
    Added Getters/Setters
    
    Added toString method

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/DTOs/UserDTO.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/DTOs/UserDTO.java
@@ -3,6 +3,7 @@ package ultranomics.enterprisefoundationsproject.DTOs;
 import ultranomics.enterprisefoundationsproject.datamodels.User;
 
 public class UserDTO {
+    
     private String userID;
     private String username;
     private String email;
@@ -11,9 +12,70 @@ public class UserDTO {
     private String role;
     
     public UserDTO(User subject){
-        //TODO add this. = import statements
+        this.userID = subject.getUserID();
+        this.username = subject.getUsername();
+        this.email = subject.getEmail();
+        this.givenName = subject.getGivenName();
+        this.surname = subject.getSurname();
+        this.role = subject.getRole();
     }
     
-    //TODO add Getters/Setters
-    //TODO add toString
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    @Override
+    public String toString() {
+        return "UserDTO{" + 
+                "\nuserID=" + userID + 
+                ", \nusername=" + username + 
+                ", \nemail=" + email + 
+                ", \ngivenName=" + givenName + 
+                ", \nsurname=" + surname + 
+                ", \nrole=" + role + '}';
+    }
 }
