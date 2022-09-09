12ee5030648f6d1ad8d7b0e5f1e41d8e74047f92
NMooney <nmooney96@hotmail.com>
2022-09-09 14:46:52 -0400

    Update DataSourceException.java
    added requirement of Throwable to first constructor and added second constructor for predefined message

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/exceptionTemplates/DataSourceException.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/exceptionTemplates/DataSourceException.java
@@ -1,7 +1,11 @@
 package ultranomics.enterprisefoundationsproject.exceptionTemplates;
 
 public class DataSourceException extends RuntimeException{
-    public DataSourceException(){
-        super("ERROR: Invalid Data Source");
+    public DataSourceException(Throwable cause){
+        super("ERROR: Invalid Data Source", cause);
+    }
+    
+    public DataSourceException(String message, Throwable cause){
+        super(message, cause);
     }
 }
