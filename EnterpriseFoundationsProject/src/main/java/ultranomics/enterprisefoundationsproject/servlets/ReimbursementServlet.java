0cd79b49dc17ca3e7f34058eac4cf50a47ed878f
NMooney <nmooney96@hotmail.com>
2022-09-13 18:47:22 -0400

    Update ReimbursementServlet.java
    Removed unneeded doDelete method and updated doGet, doPost, and doPut methods

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/servlets/ReimbursementServlet.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/servlets/ReimbursementServlet.java
@@ -1,6 +1,11 @@
 package ultranomics.enterprisefoundationsproject.servlets;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import ultranomics.enterprisefoundationsproject.services.ReimbursementService;
 
 public class ReimbursementServlet extends HttpServlet{
@@ -11,8 +16,29 @@ public class ReimbursementServlet extends HttpServlet{
         this.reimbursementServ = reimbursementServ;
     }
 
-    //TODO add doGet method
-    //TODO add doPost method
-    //TODO add doPut method
-    //TODO add doDelete method
+    //doGet:get all reimbursements(require not Employee), 
+    //      get owned reimbursements(user_ID matches author_id),
+    //      get pending reimbursements(require not employee, )
+    //TODO complete doGet method
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+    }//end of doGet method
+       
+    //doPost:create new reimbursement(author_id to equal sessionID)
+    //TODO complete doPost method
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+    }//end of doPost method
+    
+    //TODO complete doPut method
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+    }//end of doPut method
+    
 }
