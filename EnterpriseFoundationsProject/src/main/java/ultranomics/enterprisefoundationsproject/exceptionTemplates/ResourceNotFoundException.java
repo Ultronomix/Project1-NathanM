f6e86c932eeaa082a4f8eb1ec200171ffffc4291
NMooney <109111522+NMooney@users.noreply.github.com>
2022-09-13 03:16:45 -0400

    Delete EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/exceptionTemplates directory
    File was refactored to exceptiontemplates

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/exceptionTemplates/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ultranomics.enterprisefoundationsproject.exceptiontemplates;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(){
-        super("ERROR: Resource Not Found");
-    }
-}
