d20fd3c613b7b0543414f9a5a221687f85b2139a
NMooney <nmooney96@hotmail.com>
2022-09-13 02:33:27 -0400

    Update AuthenticationService.java
    Added authenticate method and relevant imports

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/services/AuthenticationService.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/services/AuthenticationService.java
@@ -1,6 +1,9 @@
 package ultranomics.enterprisefoundationsproject.services;
 
 import ultranomics.enterprisefoundationsproject.DAOs.UserDAO;
+import ultranomics.enterprisefoundationsproject.DTOs.CredentialsDTO;
+import ultranomics.enterprisefoundationsproject.DTOs.UserDTO;
+import ultranomics.enterprisefoundationsproject.exceptionTemplates.*;
 
 public class AuthenticationService {
     private final UserDAO userDAO;
@@ -10,4 +13,23 @@ public class AuthenticationService {
     }
 
     //TODO impliment
+    
+    public UserDTO authenticate(CredentialsDTO credImport){
+        if(credImport == null){
+            //will be logged at Servlet level
+            throw new InvalidRequestException("Credentials payload found to be null");
+        }
+        if(credImport.getUsername() == null || credImport.getUsername().length() == 0){
+            //will be logged at Servlet level
+            throw new InvalidRequestException("Username found to be empty/null");
+        }
+        if(credImport.getPassword() == null || credImport.getPassword().length() == 0){
+            //will be logged at Servlet level
+            throw new InvalidRequestException("Password found to be empty/null");
+        }
+        
+        return userDAO.findUserByUsernameAndPassword(credImport.getUsername(), credImport.getPassword())
+                .map(UserDTO::new)
+                .orElseThrow(AuthenticationException::new);
+    }
 }
