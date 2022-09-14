f877cc55a22de0082919bf8fb838beb411fb0f5d
NMooney <nmooney96@hotmail.com>
2022-09-14 01:17:23 -0400

    Update ReimbursementService.java
    Added validation IF logic and connected to reimbursementDAO

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/services/ReimbursementService.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/services/ReimbursementService.java
@@ -3,6 +3,8 @@ package ultranomics.enterprisefoundationsproject.services;
 import ultranomics.enterprisefoundationsproject.DAOs.ReimbursementDAO;
 import ultranomics.enterprisefoundationsproject.DTOs.ReimbursementDTO;
 import ultranomics.enterprisefoundationsproject.datainsertion.NewReimbursementInsertion;
+import ultranomics.enterprisefoundationsproject.datamodels.Reimbursement;
+import ultranomics.enterprisefoundationsproject.exceptiontemplates.InvalidRequestException;
 
 public class ReimbursementService {
     private final ReimbursementDAO reimbursementDAO;
@@ -11,10 +13,33 @@ public class ReimbursementService {
         this.reimbursementDAO = reimbursementDAO;
     }
 
-    public ReimbursementDTO generate(NewReimbursementInsertion reimbImport){
+    public ReimbursementDTO generate(NewReimbursementInsertion reimbImport) throws InvalidRequestException{
         //TODO add if logic for field validation before passing to DAO
-        //TODO pass to DAO
-        //TODO return
-    }
-    //TODO impliment
-}
+
+        if(reimbImport == null){
+            throw new InvalidRequestException("ERROR: can not register null payload");
+        }
+        if(reimbImport.getAmount() <= 0){
+            throw new InvalidRequestException("ERROR: can not register reimbursement amount less than $0.01");
+        }
+        if(reimbImport.getTimeSub() == null || reimbImport.getTimeSub().length() <= 0 ){
+            throw new InvalidRequestException("ERROR: must document day and time of expense");
+        }
+        if(reimbImport.getDescription() == null || reimbImport.getDescription().length() <= 0 ){
+            throw new InvalidRequestException("ERROR: reimbursement description must be provided");
+        }
+        if(reimbImport.getAuthorID() == null || reimbImport.getAuthorID().length() <= 0 ){
+            throw new InvalidRequestException("ERROR: authorID must be listed");
+        }
+        if(reimbImport.getStatusID() == null || reimbImport.getStatusID().length() <= 0 ){
+            throw new InvalidRequestException("ERROR: StatusID must be listed");
+        }
+        if(reimbImport.getTypeID() == null || reimbImport.getTypeID().length() <= 0 ){
+            throw new InvalidRequestException("ERROR: TypeID must be listed");
+        }
+        
+        Reimbursement target = reimbursementDAO.create(reimbImport).orElse(null);
+        ReimbursementDTO result = new ReimbursementDTO(target);
+        return result;
+    }//end generate method
+}//end ReimbursementService class
