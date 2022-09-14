ab0adc7d24e60f123be85c04157dda1c3a1740bd
NMooney <nmooney96@hotmail.com>
2022-09-13 21:07:55 -0400

    Update ReimbursementServlet.java
    implemented doPost method minus logging

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/servlets/ReimbursementServlet.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/servlets/ReimbursementServlet.java
@@ -1,11 +1,20 @@
 package ultranomics.enterprisefoundationsproject.servlets;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import ultranomics.enterprisefoundationsproject.DTOs.ReimbursementDTO;
+import ultranomics.enterprisefoundationsproject.DTOs.UserDTO;
+import ultranomics.enterprisefoundationsproject.ErrorReport;
+import ultranomics.enterprisefoundationsproject.datainsertion.NewReimbursementInsertion;
+import ultranomics.enterprisefoundationsproject.exceptiontemplates.DataSourceException;
+import ultranomics.enterprisefoundationsproject.exceptiontemplates.InvalidRequestException;
+import ultranomics.enterprisefoundationsproject.exceptiontemplates.ResourcePersistenceException;
 import ultranomics.enterprisefoundationsproject.services.ReimbursementService;
 
 public class ReimbursementServlet extends HttpServlet{
@@ -32,6 +41,52 @@ public class ReimbursementServlet extends HttpServlet{
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         ObjectMapper jsonMapper = new ObjectMapper();
         resp.setContentType("application/json");
+        
+        //HTTPS session (might not exist)
+        HttpSession userSession = req.getSession(false);
+        
+        //Confirm user is logged in
+        if(userSession == null){
+            resp.setStatus(401);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorReport(401, "ERROR 401: Authorization Missing")));
+            return;
+        }
+        
+        //Gathering submitted username to be verified for authorization
+        String usernameSubmission = req.getParameter("username");
+        
+        //userSession set in AuthenticationServlet which sets "authUser" after someone has logged in
+        UserDTO requester = (UserDTO) userSession.getAttribute("authUser");
+        
+        if(!usernameSubmission.equals(userSession.getAttribute("authUser"))){
+            resp.setStatus(403);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorReport(403, "ERROR 403: Authorization Insufficent to Access")));
+        }
+        
+        try{
+            //Servlet layer new Reimbursement creation
+            //reimbursementServ.register is passed reimbusement to insert, will return with 
+            //ResourceCreationDTO to confirm new User was created
+            NewReimbursementInsertion requestBody = jsonMapper.readValue(req.getInputStream(), NewReimbursementInsertion.class);
+            ReimbursementDTO responseBody = reimbursementServ.generate(requestBody);
+            resp.getWriter().write(jsonMapper.writeValueAsString(responseBody));
+            
+        } catch (InvalidRequestException | JsonMappingException e) {
+            //TODO add logging based on 9/9 lecture
+            resp.setStatus(400); // BAD REQUEST
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorReport(400, e.getMessage())));
+
+        } catch (ResourcePersistenceException e) {
+            //TODO add logging based on 9/9 lecture
+            resp.setStatus(409); // CONFLICT; indicates that the provided resource could not be saved without conflicting with other data
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorReport(409, e.getMessage())));
+
+        } catch (DataSourceException e) {
+            //TODO add logging based on 9/9 lecture
+            resp.setStatus(500); // INTERNAL SERVER ERROR; general error indicating a problem with the server
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorReport(500, e.getMessage())));
+
+        }
     }//end of doPost method
     
     //TODO complete doPut method
