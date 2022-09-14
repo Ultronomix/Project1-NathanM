e37bed309af2e75d3d6c9cf991ade5f696a370e0
NMooney <nmooney96@hotmail.com>
2022-09-13 21:06:35 -0400

    Update ReimbursementDAO.java
    initial development of base select and create method

--- EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/DAOs/ReimbursementDAO.java
+++ EnterpriseFoundationsProject/src/main/java/ultranomics/enterprisefoundationsproject/DAOs/ReimbursementDAO.java
@@ -1,5 +1,17 @@
 package ultranomics.enterprisefoundationsproject.DAOs;
 
+import ultranomics.enterprisefoundationsproject.datainsertion.NewReimbursementInsertion;
+import ultranomics.enterprisefoundationsproject.datamodels.Reimbursement;
+
 public class ReimbursementDAO {
+    String baseSelect = "SELECT ER.reimb_id, ER.amount, ER " +
+                                      "FROM ERS_REIMBURSEMENTS ER " +
+                                      "JOIN ERS_USERS EU " +
+                                      "ON EU.role_id = EUR.role_id ";
+
     //TODO impliment
+    public Reimbursement create(NewReimbursementInsertion reimbImport){
+        
+        //TODO return
+    }
 }
