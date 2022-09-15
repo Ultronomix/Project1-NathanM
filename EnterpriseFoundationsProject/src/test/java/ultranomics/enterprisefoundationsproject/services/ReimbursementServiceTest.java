d25b3b775fbe0c96c490288b758b32c19b299426
NMooney <nmooney96@hotmail.com>
2022-09-15 19:41:20 -0400

    ServiceTest Update
    started and documented TODOs for ReimbursementServiceTest and UserServiceTest classes

--- EnterpriseFoundationsProject/src/test/java/ultranomics/enterprisefoundationsproject/services/ReimbursementServiceTest.java
+++ EnterpriseFoundationsProject/src/test/java/ultranomics/enterprisefoundationsproject/services/ReimbursementServiceTest.java
@@ -1,14 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ultranomics.enterprisefoundationsproject.services;
 
-/**
- *
- * @author nmoon
- */
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import ultranomics.enterprisefoundationsproject.DAOs.ReimbursementDAO;
+
 public class ReimbursementServiceTest {
+    ReimbursementService sut; //what we are testing SUT(System Under Test)
+    ReimbursementDAO mockReDAO; //mock injections, never the SUT
+    
+    @BeforeEach
+    public void setup(){
+        mockReDAO = Mockito.mock(ReimbursementDAO.class);
+        sut = new ReimbursementService(mockReDAO); 
+    }
+    
+    @AfterEach
+    public void cleanUp(){
+        Mockito.reset(mockReDAO);//reset method counters for verify counting
+    }
+    
+    //TODO test generate
+    
+    //TODO test getAllReimbs
+    
+    //TODO test getAllPendingReimbs
+    
+    //TODO test getOwnedReimbs
+    
+    //TODO test getOwnedPendingReimbs
+    
     
 }
--- EnterpriseFoundationsProject/src/test/java/ultranomics/enterprisefoundationsproject/services/UserServiceTest.java
+++ EnterpriseFoundationsProject/src/test/java/ultranomics/enterprisefoundationsproject/services/UserServiceTest.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ultranomics.enterprisefoundationsproject.services;
 
-/**
- *
- * @author nmoon
- */
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import ultranomics.enterprisefoundationsproject.DAOs.UserDAO;
+
 public class UserServiceTest {
+    UserService sut; //what we are testing SUT(System Under Test)
+    UserDAO mockUserDAO; //mock injections, never the SUT
     
+    @BeforeEach
+    public void setup(){
+        mockUserDAO = Mockito.mock(UserDAO.class);
+        sut = new UserService(mockUserDAO); 
+    }
+    
+    @AfterEach
+    public void cleanUp(){
+        Mockito.reset(mockUserDAO);//reset method counters for verify counting
+    }
+    
+    //TODO test getAllUsers
+    
+    //TODO test getUserByUsername
+    
+    //TODO test register
+    
+    //TODO test updateUser(not yet written)
+    
+    //TODO test deactivate
 }
